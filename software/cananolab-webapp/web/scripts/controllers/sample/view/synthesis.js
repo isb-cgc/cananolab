c397aabfb139af8e45650db538fa7b67fc2bac23
Shaun Einolf <einolfs@mail.nih.gov>
2019-06-10 12:33:38 -0400

    modify gitignore

---
+++ software/cananolab-webapp/web/scripts/controllers/sample/view/synthesis.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+var app = angular.module('angularApp')
+
+app.controller('SynthesisCtrl', function (sampleService,utilsService,navigationService, groupService, $rootScope,$scope,$http,$location,$filter,$routeParams) {
+});
