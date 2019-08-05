00925970d78cb1bc08f3e883eee958ca7da458ff
Tracy <safrant@mail.nih.gov>
2019-08-05 18:19:28 -0400

    Update header to Leidos
    Find and replace to update header from SAIC to Leidos

--- software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/table/TableCell.java
+++ software/cananolab-webapp/src/gov/nih/nci/cananolab/dto/common/table/TableCell.java
@@ -1,6 +1,6 @@
 /*
- *  Copyright SAIC
- *  Copyright SAIC-Frederick
+ *  Copyright Leidos
+ *  Copyright Leidos Biomedical
  *
  *  Distributed under the OSI-approved BSD 3-Clause License.
  *  See http://ncip.github.com/cananolab/LICENSE.txt for details.
