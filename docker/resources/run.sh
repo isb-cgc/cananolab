7243f55d38c1f915bcfec04391226e3552f0e90a
Shaun Einolf <einolfs@mail.nih.gov>
2019-10-10 12:47:51 -0400

    add modules cli

--- docker/resources/run.sh
+++ docker/resources/run.sh
@@ -8,9 +8,11 @@ cd /usr/local/cananolab/software/cananolab-webapp/target/dist/
 
 cp caNanoLab.war /local/content/caNanoLab/artifacts
 cd /usr/local/cananolab/software/cananolab-webapp/target/dist/common
-cp csmapi-5.2.jar bcprov-jdk15on-1.47.jar mysql-connector-java-5.1.26.jar caNanoLab_modules.cli caNanoLab_setup.cli caNanoLab_deploy.cli /local/content/caNanoLab/artifacts
+cp bcprov-jdk15on-1.47.jar mysql-connector-java-5.1.26.jar caNanoLab_modules.cli caNanoLab_setup.cli caNanoLab_deploy.cli /local/content/caNanoLab/artifacts
 
-cp ApplicationSecurityConfig.xml wikihelp.properties /local/content/caNanoLab/config
+cp wikihelp.properties /local/content/caNanoLab/config
 
+/opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
+/opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
 /opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_deploy.cli
 echo "DEPLOYED"
