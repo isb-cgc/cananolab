a6776b6c0d6fb9716505d9955a262a88def0c222
Shaun Einolf <seinolf@gmail.com>
2020-03-23 20:59:04 -0400

    adding some material edit form stuff

--- software/cananolab-webapp/web/scripts/controllers/sample/edit/synthesis/editSynthesis.js
+++ software/cananolab-webapp/web/scripts/controllers/sample/edit/synthesis/editSynthesis.js
@@ -1,29 +1,40 @@
 'use strict';
 
 var app = angular.module('angularApp')
-.controller('EditSynthesisCtrl', function (sampleService,utilsService,navigationService, groupService, $rootScope,$scope,$http,$location,$filter,$modal,$routeParams) {
-    $rootScope.tabs = navigationService.get();
-    $rootScope.groups = groupService.getGroups.data.get();
-    $scope.sampleData = sampleService.sampleData;
-    $scope.sampleId = sampleService.sampleId;
+  .controller('EditSynthesisCtrl', function (sampleService, utilsService, navigationService, groupService, $rootScope, $scope, $http, $location, $filter, $modal, $routeParams) {
+    $scope.sampleName = '';
+    $scope.sampleId = $location.search()['sampleId'];  
 
-    // Displays left hand nav for samples section. navTree shows nav and navDetail is page index //
-    $scope.sampleData = sampleService.sampleData;
+    $scope.materials = [];
+    $scope.functionalizations = [];
+    $scope.purifications = [];
+    $scope.showForm = false;
+    $scope.openMaterialEditForm = function () {
+      if ($scope.showForm == false) {
+        $scope.showForm = true;
+      }
+      else {
+        $scope.showForm = false;
+      };
+    };
 
-    if ($routeParams.isAdvancedSearch) {
-      $scope.isAdvancedSearch = 1;
-    };   
+    $scope.loader = true;
 
-     $scope.goBack = function() {
-        if ($scope.isAdvancedSearch) {
-          $location.path("/advancedSampleSearch").replace();           
-        }
-        else {
-          $location.path("/sampleResults").replace();           
-        }
-      $location.search('sampleId', null);      };
-      
-    if ($routeParams.sampleId) {
-      $scope.sampleId.data = $routeParams.sampleId;
-    };  
-});
+    $http({ method: 'GET', url: '/caNanoLab/rest/synthesis/summaryView?sampleId=' + $scope.sampleId }).
+      success(function (data, status, headers, config) {
+        $scope.sampleName = sampleService.sampleName($scope.sampleId);
+        $scope.materials = data.synthesisMaterials;
+        $scope.functionalizations = data.synthesisFunctionalization;
+        $scope.purifications = data.synthesisPurification;
+        $scope.loader = false;
+      });
+
+    $http({ method: 'GET', url: '/caNanoLab/rest/sample/getCurrentSampleName?sampleId=' + $scope.sampleId }).
+      success(function (data, status, headers, config) {
+        $scope.sampleName = sampleService.sampleName($scope.sampleId);
+        $scope.materials = data.synthesisMaterials;
+        $scope.functionalizations = data.synthesisFunctionalization;
+        $scope.purifications = data.synthesisPurification;
+        $scope.loader = false;
+      });      
+  });
