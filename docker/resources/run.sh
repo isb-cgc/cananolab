2a45decd50ce06895271b88dcbe5cbe13e5762d8
Shaun Einolf <seinolf@gmail.com>
2021-11-09 10:53:42 -0500

    adding update.cli

--- docker/resources/run.sh
+++ docker/resources/run.sh
@@ -18,5 +18,29 @@ cp wikihelp.properties /local/content/caNanoLab/config
 
 /opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
 /opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
+#!/bin/bash
+rm -rf /local/content/caNanoLab/artifacts/*
+rm -rf /local/content/caNanoLab/config/*
+rm -rf /usr/local/cananolab/software/cananolab-webapp/target/dist
+rm -rf /opt/wildfly-8.2.1.Final/modules/com/mysql
+rm -rf /opt/wildfly-8.2.1.Final/standalone/deployments/*
+cd /usr/local/cananolab/software/cananolab-webapp/
+ant dist
+cd /usr/local/cananolab/software/cananolab-webapp/target/dist/
+cp /usr/local/cananolab/docker/resources/standalone-full.xml /opt/wildfly-8.2.1.Final/standalone/configuration
+cp caNanoLab.war /local/content/caNanoLab/artifacts
+cd /usr/local/cananolab/software/cananolab-webapp/lib/sdk
+
+cp bcprov-jdk15on* csmapi* mysql-connector-java* /local/content/caNanoLab/artifacts
+cd /usr/local/cananolab/software/cananolab-webapp/target/dist/common
+cp caNanoLab_modules.cli caNanoLab_setup.cli caNanoLab_deploy.cli /local/content/caNanoLab/artifacts
+cp wikihelp.properties /local/content/caNanoLab/config
+
+/opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
+/opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
+/opt/wildfly-8.2.1.Final/bin/jboss-cli.sh --file=/usr/local/cananolab/software/cananolab-webapp/db-scripts/2.4.0/caNano_Update_mysql.cli
+
+cp /local/content/caNanoLab/artifacts/caNanoLab.war /opt/wildfly-8.2.1.Final/standalone/deployments
+echo "DEPLOYED"
 cp /local/content/caNanoLab/artifacts/caNanoLab.war /opt/wildfly-8.2.1.Final/standalone/deployments
 echo "DEPLOYED"
