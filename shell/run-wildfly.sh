7cbae1828c58dd86289cdf825ee393c8317000b8
S. Paquette <spaquett@systemsbiology.org>
2021-12-17 11:14:14 -0800

    -> Entrypoint fix

--- shell/run-wildfly.sh
+++ shell/run-wildfly.sh
@@ -1,5 +1,5 @@
-./jboss-cli.sh --file=content/caNanoLab/artifacts/caNanoLab_modules.cli
-./jboss-cli.sh --file=content/caNanoLab/artifacts/caNanoLab_setup.cli
-./jboss-cli.sh --file=content/caNanoLab/artifacts/caNanoLab_deploy.cli
+./jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
+./jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
+./jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_deploy.cli
 
 ./standalone.sh --server-config=standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0
