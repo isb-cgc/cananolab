5c8b1ea82096f6ae39c503cefbeb60c2406ffdf4
S. Paquette <spaquett@systemsbiology.org>
2021-12-17 10:31:48 -0800

    -> app yaml naming

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -1,3 +1,3 @@
-gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_APP_YAML}" ./
+gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_APP_YAML}" ./app.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_RUNTIME_SA_KEY}" ./privatekey.json
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${BUILD_PROPERTIES}" ./software/cananolab-webapp/build.properties
