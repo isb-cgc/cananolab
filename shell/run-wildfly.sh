0109adebd14599fc8fa8bc750fef08feaea8d971
S. Paquette <spaquett@systemsbiology.org>
2021-12-17 19:36:41 -0800

    -> JBoss timing

--- shell/run-wildfly.sh
+++ shell/run-wildfly.sh
@@ -6,26 +6,23 @@ cd /opt/wildfly-8.2.1.Final/bin
 
 echo "Waiting while JBoss starts..."
 
-MAX_CHECKS=5
-check_counter=0
+counter=0
 result=`./jboss-cli.sh -c --commands="read-attribute server-state" > out 2&1`
 echo "$result" | grep -q "running"
-while [ $? -ne 0 ] && [ check_counter < MAX_CHECKS ]; do
+while [ $? -ne 0 ] && [ $counter -lt 5 ]; do
   result=`./jboss-cli.sh -c --commands="read-attribute server-state" > out 2&1`
   echo "$result" | grep -q "running"
-  check_counter = check_counter+1
+  ((counter=counter+1))
   sleep 6
 done
 
-echo $?
-
-if [ $? -eq 0 ];then
+if [ $? -eq 0 ]; then
    echo "Jboss running - continuing setup and deployment."
   ./jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
   ./jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
   ./jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_deploy.cli
 else
   echo "Didn't see JBoss start within 30 seconds!"
-  exit(1)
+  exit 1
 fi
 
