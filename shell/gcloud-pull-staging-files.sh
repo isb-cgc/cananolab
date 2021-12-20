115ceaec436451d33d857256ce323ceea797c580
S. Paquette <spaquett@systemsbiology.org>
2021-12-19 17:19:24 -0800

    -> dependency joys

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -2,4 +2,5 @@ gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_APP_YAML}" ./app.yaml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_RUNTIME_SA_KEY}" ./privatekey.json
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${BUILD_PROPERTIES}" ./software/cananolab-webapp/build.properties
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/standalone-full.xml" ./standalone-full.xml
+gsutil cp "gs://${DEPLOYMENT_BUCKET}/maven-settings.xml" ./
 
