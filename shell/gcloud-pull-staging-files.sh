1127bdfce409e7fb72d250928d8705b1cfbff910
S. Paquette <spaquett@systemsbiology.org>
2022-01-03 20:39:32 -0800

    -> Move jars into bucket

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -3,4 +3,5 @@ gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_RUNTIME_SA_KEY}" ./privatekey.json
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${BUILD_PROPERTIES}" ./software/cananolab-webapp/build.properties
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/standalone-full.xml" ./standalone-full.xml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/maven-settings.xml" ./
-gsutil cp "gs://${DEPLOYMENT_BUCKET}/jars/*" ./jars/
+mkdir jars
+gsutil cp "gs://${DEPLOYMENT_BUCKET}/jars/*" ./jars
