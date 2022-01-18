648b304c6f4f61c1d833c852d3f50329790eaea3
S. Paquette <spaquett@systemsbiology.org>
2022-01-18 10:13:06 -0800

    -> Add restart as final step.

--- shell/run-wildfly.sh
+++ shell/run-wildfly.sh
@@ -3,13 +3,14 @@
 export $(cat /local/content/.env | grep -v ^# | xargs) 2> /dev/null
 
 export WILDFLY_BIN=/opt/wildfly-8.2.1.Final/bin
+export JBOSS_CLI=$WILDFLY_BIN/jboss-cli.sh
 
 ${WILDFLY_BIN}/standalone.sh --server-config=standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0 &
 
 echo "Waiting while JBoss starts:"
 
 counter=0
-result=`${WILDFLY_BIN}/jboss-cli.sh -c --commands="read-attribute server-state"`
+result=`${JBOSS_CLI} -c --commands="read-attribute server-state"`
 echo "JBoss status: ${result}"
 echo "$result" | grep -q "running"
 while [ $? -ne 0 ] && [ $counter -lt 5 ]; do
@@ -23,19 +24,23 @@ done
 if [ $? -eq 0 ]; then
   echo "JBoss is now running - continuing setup and deployment:"
   echo "Setting up logging."
-  ${WILDFLY_BIN}/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_logging.cli
+  ${JBOSS_CLI} --file=/local/content/caNanoLab/artifacts/caNanoLab_logging.cli
   echo "Adding admin console user."
   ${WILDFLY_BIN}/add-user.sh -a -u "${WILDFLY_ADMIN}" -p "${WILDFLY_ADMIN_PASSWORD}" -g "admin"
   echo "Adding BouncyCastle and JDBC driver to Wildfly"
-  ${WILDFLY_BIN}/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
+  ${JBOSS_CLI} --file=/local/content/caNanoLab/artifacts/caNanoLab_modules.cli
   echo "Setting up data sources"
-  ${WILDFLY_BIN}/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
+  ${JBOSS_CLI} --file=/local/content/caNanoLab/artifacts/caNanoLab_setup.cli
   echo "Testing data source setup and connection"
-  ${WILDFLY_BIN}/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_checks.cli
+  ${JBOSS_CLI} --file=/local/content/caNanoLab/artifacts/caNanoLab_checks.cli
   echo "Deploying caNano WAR"
-  ${WILDFLY_BIN}/jboss-cli.sh --file=/local/content/caNanoLab/artifacts/caNanoLab_deploy.cli
+  ${JBOSS_CLI} --file=/local/content/caNanoLab/artifacts/caNanoLab_deploy.cli
 else
   echo "Didn't see JBoss start within 30 seconds!"
   exit 1
 fi
 
+echo "Restarting Wildfly"
+${JBOSS_CLI} -c --controller=localhost:9990 ":shutdown"
+
+${WILDFLY_BIN}/standalone.sh --server-config=standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0
