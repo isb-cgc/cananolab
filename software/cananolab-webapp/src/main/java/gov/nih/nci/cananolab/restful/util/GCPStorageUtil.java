65842f48e6b4da3c229b023701b1e71519422f62
mitianisb <mtian@systemsbiology.org>
2022-03-21 12:56:07 -0700

    Add print all system vars to debug

--- software/cananolab-webapp/src/main/java/gov/nih/nci/cananolab/restful/util/GCPStorageUtil.java
+++ software/cananolab-webapp/src/main/java/gov/nih/nci/cananolab/restful/util/GCPStorageUtil.java
@@ -4,6 +4,8 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import gov.nih.nci.cananolab.exception.FileException;
 
+import java.util.Map;
+
 public class GCPStorageUtil {
     private static Storage gcpStorageService = null;
 
@@ -22,10 +24,28 @@ public class GCPStorageUtil {
     }
 
     public static String getGCPStorageBucketPath() {
-        return System.getenv("GCP_STORAGE_BUCKET_PATH");
+        String bucketPath = System.getenv("GCP_STORAGE_BUCKET_PATH");
+        System.out.println("GCPStorageUtil get bucket path: " + bucketPath);
+        if (bucketPath == "" || bucketPath == null) {
+            printAllSystemVars();
+        }
+        return bucketPath;
+    }
+
+    private static String printAllSystemVars() {
+        String allVars = "";
+        for (Map.Entry<String,String> entry : System.getenv().entrySet()) {
+            allVars += String.format("Key: %s, value: %s\n", entry.getKey(), entry.getValue());
+        }
+        System.out.println("GCPStorageUtil all vars: " + allVars);
     }
 
     public static String getGCPStorageRootFolderPath() {
-        return System.getenv("GCP_STORAGE_ROOT_FOLDER_PATH");
+        String folderPath = System.getenv("GCP_STORAGE_ROOT_FOLDER_PATH");
+        System.out.println("GCPStorageUtil get folder path: " + folderPath);
+        if (folderPath == "" || folderPath == null) {
+            printAllSystemVars();
+        }
+        return folderPath;
     }
 }
