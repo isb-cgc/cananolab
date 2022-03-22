d73111c8a6625bf54ca425a013bcae7999bfe2fb
S. Paquette <spaquett@systemsbiology.org>
2022-03-22 12:17:44 -0700

    -> Test build

--- shell/start-wildfly.sh
+++ shell/start-wildfly.sh
@@ -9,4 +9,4 @@ export WILDFLY_BIN=$WILDFLY_HOME/bin
 # It will then shut down Wildfly so we can start it up here.
 /local/content/wildfly-setup.sh
 
-${WILDFLY_BIN}/standalone.sh --Dapp.props.path=${APPLICATION_PROPERTIES_PATH} --server-config=standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0
+${WILDFLY_BIN}/standalone.sh -Dapp.props.path=${APPLICATION_PROPERTIES_PATH} --server-config=standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0
