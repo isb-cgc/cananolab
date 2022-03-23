81b1b5f953198779df81a1dc99f90b3bccf7cd07
S. Paquette <spaquett@systemsbiology.org>
2022-03-23 10:47:54 -0700

    -> Moving to root context

--- shell/start-wildfly.sh
+++ shell/start-wildfly.sh
@@ -7,6 +7,6 @@ export WILDFLY_BIN=$WILDFLY_HOME/bin
 
 # Call the setup script which will ensure we have the right logging, modules, and run the deployment.
 # It will then shut down Wildfly so we can start it up here.
-/local/content/wildfly-setup.sh
+# /local/content/wildfly-setup.sh
 
 ${WILDFLY_BIN}/standalone.sh -Dapp.props.path=${APPLICATION_PROPERTIES_PATH} --server-config=standalone-full.xml -b 0.0.0.0 -bmanagement 0.0.0.0
