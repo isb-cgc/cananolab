c7a204a7f5a6c130b3c528c0afcf5d0784328fdc
Your Name <you@example.com>
2022-03-25 08:07:01 -0400

    XML and JSON export change get to post

--- software/cananolab-webapp/web/scripts/controllers/sample/edit/editSample.js
+++ software/cananolab-webapp/web/scripts/controllers/sample/edit/editSample.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = angular.module('angularApp')
-    .controller('editSampleCtrl', function (sampleService, navigationService, groupService, $scope, $rootScope, $filter, ngTableParams, $http, $location, $modal, $routeParams) {
+    .controller('editSampleCtrl', function (sampleService, navigationService, groupService, $scope, utilsService, $rootScope, $filter, ngTableParams, $http, $location, $modal, $routeParams) {
         $rootScope.tabs = navigationService.get();
         $rootScope.groups = groupService.getGroups.data.get();
         $scope.sampleResultData = sampleService.sampleData;
@@ -60,11 +60,16 @@ var app = angular.module('angularApp')
 
         $scope.exportJson = function () {
             $scope.loader = true;
+
+            let postObject = new Object();
+            postObject.sampleIds = $scope.sampleId.data;
             $http({
-                method: 'GET',
                 url: '/caNanoLab/rest/sample/fullSampleExportJsonAll',
-                params: {
-                    "sampleIds": $scope.sampleId.data
+                dataType: 'json',
+                method: 'POST',
+                data: postObject,
+                headers: {
+                    "Content-Type": "application/json"
                 }
             }).
             then(function (res) {
@@ -88,14 +93,17 @@ var app = angular.module('angularApp')
 
         $scope.exportXml = function () {
             $scope.loader = true;
+            let postObject = new Object();
+            postObject.sampleIds = $scope.sampleId.data;
             $http({
-                method: 'GET',
                 url: '/caNanoLab/rest/sample/fullSampleExportXmlAll',
-                params: {
-                    "sampleIds": $scope.sampleId.data
+                dataType: 'json',
+                method: 'POST',
+                data: postObject,
+                headers: {
+                    "Content-Type": "application/json"
                 }
-            }).
-            then(function (res) {
+            }).then(function (res) {
                 res = res['data']
                 let a = (window).document.createElement('a');
                 a.href = (window).URL.createObjectURL(new Blob([JSON.stringify(res)], {
