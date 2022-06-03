bfcba5ba9982043d1828d7130235e83c1bb7233b
mitianisb <mtian@systemsbiology.org>
2022-06-02 21:03:01 -0700

    Try update wildFly from 13 to 23

--- software/deploy-change.sh
+++ software/deploy-change.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-8.jdk/Contents/Home
-JBOSS=/users/mi/wildfly-13.0.0.Final/bin/jboss-cli.sh
+JBOSS=/users/mi/wildfly-24.0.1.Final/bin/jboss-cli.sh
 $JBOSS --file=/Users/mi/software/local/content/artifacts/caNanoLab_modules.cli
 $JBOSS --file=/Users/mi/software/local/content/artifacts/caNanoLab_setup.cli
 $JBOSS --file=/Users/mi/software/local/content/artifacts/caNanoLab_deploy.cli
