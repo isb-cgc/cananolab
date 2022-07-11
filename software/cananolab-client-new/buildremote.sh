210459b9735f82b6f17bf675cfb0812fa4504519
regulator95 <einolfs@nih.gov>
2022-07-11 13:25:21 -0400

    adding cli script and changing from new client to root.war

--- software/cananolab-client-new/buildremote.sh
+++ software/cananolab-client-new/buildremote.sh
@@ -3,5 +3,5 @@ npm install
 rm -rf '/local/home/wildfly8a/cananolab_new_client'
 ng build --base-href '.' --configuration 'production' --output-path '/local/home/wildfly8a/cananolab_new_client'
 cd '/local/home/wildfly8a/cananolab_new_client'
-jar -cvf cananolab_new_client.war *
-cp cananolab_new_client.war /local/content/caNanoLab/artifacts/
+jar -cvf ROOT.war *
+cp ROOT.war /local/content/caNanoLab/artifacts/
