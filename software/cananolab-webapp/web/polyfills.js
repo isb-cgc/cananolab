82f5282b3c1d5e32a87b6aed61745be6d04d4a0e
mitianisb <mtian@systemsbiology.org>
2022-07-20 12:21:17 -0700

    update built front end in web/

--- software/cananolab-webapp/web/polyfills.js
+++ software/cananolab-webapp/web/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! c:\Users\spaquett\IdeaProjects\cananolab\software\cananolab-client-new\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /Users/mi/software/cananolab/software/cananolab-client-new/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
