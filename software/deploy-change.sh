dfacd58a69db9e0e53391fb97d1387e3768a4290
wlongabaugh <wlongabaugh@systemsbiology.org>
2022-12-01 23:32:56 -0800

    Revert common files for 196

--- software/deploy-change.sh
+++ software/deploy-change.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-ARTIFACTS=/Users/bill/WildFly/local/caNanoLab/artifacts
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/temurin-8.jdk/Contents/Home
-JBOSS=/Users/bill/WildFly/wildfly-23.0.2.Final/bin/jboss-cli.sh
-$JBOSS --file=${ARTIFACTS}/caNanoLab_modules.cli
-$JBOSS --file=${ARTIFACTS}/caNanoLab_setup.cli
-$JBOSS --file=${ARTIFACTS}/caNanoLab_deploy.cli
+JBOSS=/users/mi/wildfly-23.0.2.Final/bin/jboss-cli.sh
+$JBOSS --file=/Users/mi/software/local/content/artifacts/caNanoLab_modules.cli
+$JBOSS --file=/Users/mi/software/local/content/artifacts/caNanoLab_setup.cli
+$JBOSS --file=/Users/mi/software/local/content/artifacts/caNanoLab_deploy.cli
