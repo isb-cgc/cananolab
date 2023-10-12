5843e51f525ba099b3aa6d022de1a2be7548ab2e
wlongabaugh <wlongabaugh@systemsbiology.org>
2023-10-12 14:59:13 -0400

    Debug login failure

--- software/cananolab-webapp/src/main/java/gov/nih/nci/cananolab/security/utils/LoginFailureHandler.java
+++ software/cananolab-webapp/src/main/java/gov/nih/nci/cananolab/security/utils/LoginFailureHandler.java
@@ -30,6 +30,7 @@ public class LoginFailureHandler implements AuthenticationFailureHandler
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
 		String failureMessage = getFailureReason(request, request.getParameter("username"));
+		System.out.println("failureMessage " + failureMessage);
 		OutputStream out = response.getOutputStream();
 		out.write(failureMessage.getBytes());
 	}
@@ -39,8 +40,10 @@ public class LoginFailureHandler implements AuthenticationFailureHandler
 			UserAccountBO userAccountBO = (UserAccountBO) SpringApplicationContext.getBean(request, "userAccountBO");
 
 			CananoUserDetails userDetails = userAccountBO.readUserAccount(userName);
+			System.out.println("get Failure Reason: have user details");
 
 			boolean isAccountEnabled = userDetails.isEnabled();
+			System.out.println("is account enabled " + isAccountEnabled);
 
 			if (isAccountEnabled) {
 				return "Login failed: Incorrect user name or password.";
@@ -49,26 +52,35 @@ public class LoginFailureHandler implements AuthenticationFailureHandler
 				// If user account is disabled, we need to find out why
 				boolean isPasswordExpired = false;
 				List<PasswordHistory> histories = userAccountBO.getPasswordHistory(userName);
+				System.out.println("history list " + histories);
 				if (histories.size() > 0) {
+					System.out.println("history list size " + histories.size());
 					PasswordHistory last = histories.get(histories.size() - 1);
+					System.out.println("history last " + last);
 					if (last.getExpiryDate().isBefore(LocalDateTime.now())) {
 						// Most recent password has expired, so account was disabled
 						isPasswordExpired = true;
 					}
+					System.out.println("history expiry " + last.getExpiryDate());
 				}
 
 				// Check if inactive for too long
 				LocalDateTime lastLogin = userAccountBO.getLastLogin(userName);
+				System.out.println("last login " + lastLogin);
 				LocalDateTime inactiveAccountDate = lastLogin.plusDays(UserSelfManageServices.MAX_INACTIVE_PERIOD_DAYS);
+				System.out.println("inactiveAccountDate " + inactiveAccountDate);
 				boolean isAccountInactive = inactiveAccountDate.isBefore(LocalDateTime.now());
+				System.out.println("isAccountInactive " + isAccountInactive);
 
 				// Check if does not have an email
 				boolean hasNoEmail = userDetails.getEmailId() == null || userDetails.getEmailId().isEmpty();
+				System.out.println("hasNoEmail " + hasNoEmail);
 
 				if (hasNoEmail || isAccountInactive) {
 					return "Login failed: Account has been deactivated due to inactivity. Please contact us at caNanoLab-Support@isb-cgc.org.";
 				}
 
+				System.out.println("isPasswordExpired " + isPasswordExpired);
 				if (isPasswordExpired) {
 					return "Login failed: Last password has expired. Please reset your password.";
 				}
@@ -76,6 +88,8 @@ public class LoginFailureHandler implements AuthenticationFailureHandler
 				return "Login failed: Account has been deactivated by admin. Please contact us at caNanoLab-Support@isb-cgc.org.";
 			}
 		} catch (Exception e) {
+			System.out.println("Exception caught " + e);
+			System.out.println("e message " + e.getMessage());
 			return e.getMessage();
 		}
 	}
