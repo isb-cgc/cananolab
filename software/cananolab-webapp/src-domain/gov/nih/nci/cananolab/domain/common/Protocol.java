f622156d13f4650f1e98d210ccf22d71814649e1
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-04-11 14:23:23 -0700

    setDOI will need to be here once the getters and setters are generated

--- software/cananolab-webapp/src-domain/gov/nih/nci/cananolab/domain/common/Protocol.java
+++ software/cananolab-webapp/src-domain/gov/nih/nci/cananolab/domain/common/Protocol.java
@@ -194,7 +194,7 @@ public class Protocol  implements Serializable
 	}
 			
 	/**
-	* Compares <code>obj</code> to it self and returns true if they both are same
+	* Compares <code>obj</code> to itself and returns true if they both are same
 	*
 	* @param obj
 	**/
